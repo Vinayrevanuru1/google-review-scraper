916506098c70a42cda8842f45ed27dd6468b7e22
Vinay Kumar Revanuru <112097526+Vinayrevanuru1@users.noreply.github.com>
2024-07-23 05:03:01 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import argparse
 from google_maps_scraper import scrape_google_maps_reviews, parse_google_maps_reviews, save_reviews_to_csv
 
-# Define the restaurant URL and name
-url = r"https://www.google.com/maps/place/Maguire's/@33.3011198,-84.5571668,17z/data=!4m8!3m7!1s0x88f494dcba4b9453:0x8c5c1f45f32681e0!8m2!3d33.3011153!4d-84.5545865!9m1!1b1!16s%2Fg%2F1td019vt?entry=ttu"
-restaurant_name = 'maguires-family-restaurant'  # Enter the name of the restaurant here
+def main(url, restaurant_name):
+    # Scrape the reviews
+    html = scrape_google_maps_reviews(url)
 
-# Scrape the reviews
-html = scrape_google_maps_reviews(url)
+    # Parse the reviews
+    reviews = parse_google_maps_reviews(html)
 
-# Parse the reviews
-reviews = parse_google_maps_reviews(html)
+    # Save the reviews to a CSV file
+    save_reviews_to_csv(reviews, restaurant_name)
 
-# Save the reviews to a CSV file
-save_reviews_to_csv(reviews, restaurant_name)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Scrape Google Maps reviews.')
+    parser.add_argument('--url', type=str, required=True, help='The URL of the Google Maps page.')
+    parser.add_argument('--name', type=str, required=True, help='The name of the restaurant.')
+    args = parser.parse_args()
+    
+    main(args.url, args.name)
